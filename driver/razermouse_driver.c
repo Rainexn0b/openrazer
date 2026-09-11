5c5e2b12007dadade1077ed7e960e376889b366d
Eduaro Silva <eduardo@eduaro.silva.home>
2026-09-11 02:15:13 +0100

    remove razer_mouse_scroll_mode_version function (not used)

--- driver/razermouse_driver.c
+++ driver/razermouse_driver.c
@@ -2877,24 +2877,6 @@ static ssize_t razer_attr_read_dpi(struct device *dev, struct device_attribute *
 }
 
 /**
- * Get the scroll wheel mode protocol version for the device
- *
- * v1: tactile, free-spin
- * v2: tactile, free-spin, precision tactile
- */
-static unsigned int razer_mouse_scroll_mode_version(struct razer_mouse_device *device)
-{
-    switch (device->usb_pid) {
-    case USB_DEVICE_ID_RAZER_NAGA_V3_PRO_WIRED:
-    case USB_DEVICE_ID_RAZER_NAGA_V3_PRO_WIRELESS:
-        return 2;
-
-    default:
-        return 1;
-    }
-}
-
-/**
  * Write device file "scroll_mode"
  *
  * Sets the scroll mode of the mouse.
@@ -2905,7 +2887,6 @@ static ssize_t razer_attr_write_scroll_mode(struct device *dev, struct device_at
     struct razer_report request = {0};
     struct razer_report response = {0};
     unsigned int scroll_mode;
-    unsigned int scroll_mode_version = razer_mouse_scroll_mode_version(device);
     unsigned int scroll_mode_max;
     int err;
 
